af5ba202e7634f46f55b9b3fae869dbbb368585a
RayRay <walrusray811@gmail.com>
2022-08-16 20:36:51 +0900

    set project up!
    얍!

--- src/handlers/get-school.js
+++ src/handlers/get-school.js
@@ -1,13 +1,12 @@
 const { v4: uuidv4 } = require("uuid");
-
 exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: "이젠 눈을 떠도 행복해" + uuidv4(),
-  };
-  // All log statements are written to CloudWatch
-  console.info(
-    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
-  );
-  return response;
+    const response = {
+        statusCode: 200,
+        body: "이젠 눈을 떠도 행복해" + uuidv4(),
+    };
+    // All log statements are written to CloudWatch
+    console.info(
+        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+    );
+    return response;
 };
