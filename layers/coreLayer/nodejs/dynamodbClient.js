0851c2238f5a9ce35c7873d44a0e782e46cb5b30
RayRay <walrusray811@gmail.com>
2022-08-20 21:31:03 +0900

    add logging code for env

--- layers/coreLayer/nodejs/dynamodbClient.js
+++ layers/coreLayer/nodejs/dynamodbClient.js
@@ -8,12 +8,19 @@ const {
   AWS_SECRET_ACCESS_KEY,
 } = process.env;
 
+if (!AWS_REGION) console.warn("There's no AWS_REGION in env.");
+if (!AWS_DYNAMODB_END_POINT)
+  console.warn("There's no AWS_DYNAMODB_END_POINT in env.");
+if (!AWS_ACCESSKEY_ID) console.warn("There's no AWS_ACCESSKEY_ID in env.");
+if (!AWS_SECRET_ACCESS_KEY)
+  console.warn("There's no AWS_SECRET_ACCESS_KEY in env.");
+
 const ddbClient = new DynamoDBClient({
   region: AWS_REGION,
   endpoint: AWS_DYNAMODB_END_POINT,
   credentials: {
-    accessKeyId: AWS_ACCESSKEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    accessKeyId: AWS_ACCESSKEY_ID ?? "",
+    secretAccessKey: AWS_SECRET_ACCESS_KEY ?? "",
   },
 });
 
