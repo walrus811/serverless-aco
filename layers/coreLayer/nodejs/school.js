e38614ebee05aa45d07d433234eb65507c3b8b21
RayRay <walrusray811@gmail.com>
2022-08-24 00:30:49 +0900

    add postSchool

--- layers/coreLayer/nodejs/school.js
+++ layers/coreLayer/nodejs/school.js
@@ -1,9 +1,10 @@
 const _ = require("lodash");
 const { TABLE_NAME, PARTIAL_SCHOOL_EXP, PK_SCHOOL } = require("./contants");
-const { get, getById } = require("./crud");
+const { get, getById, post } = require("./crud");
 
 const getSchool = _.partial(get, TABLE_NAME, PARTIAL_SCHOOL_EXP, PK_SCHOOL);
 const getSchoolById = _.partial(getById, TABLE_NAME, PK_SCHOOL);
+const postSchool = _.partial(post, TABLE_NAME, PK_SCHOOL, "name", (v) => v);
 
 /**
  * @description convert dynamodb item to School
@@ -11,14 +12,15 @@ const getSchoolById = _.partial(getById, TABLE_NAME, PK_SCHOOL);
  * @return {import("./typedefs").SchoolWithId}
  */
 function getSchoolItem(item) {
-  return {
-    id: item.SK,
-    name: item.SK,
-  };
+    return {
+        id: item.SK,
+        name: item.SK,
+    };
 }
 
 module.exports = {
-  getSchool,
-  getSchoolById,
-  getSchoolItem,
+    getSchool,
+    getSchoolById,
+    getSchoolItem,
+    postSchool,
 };
