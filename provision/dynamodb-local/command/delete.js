d9ca9a20299c5f241249c74222e732f2a270659b
RayRay <walrusray811@gmail.com>
2022-08-26 00:44:34 +0900

    add delete sample code

---
+++ provision/dynamodb-local/command/delete.js
@@ -0,0 +1,19 @@
+const { ddbDocClient } = require("../client");
+const { result: tableSchema } = require("../input/table.json");
+
+(async () => {
+  try {
+    const item = {
+      PK: "School",
+      SK: `라라라고`,
+      test: "abc",
+    };
+    const data = await ddbDocClient.delete({
+      TableName: tableSchema.TableName,
+      Key: item,
+    });
+    console.log(data);
+  } catch (err) {
+    console.log("Error", err);
+  }
+})();
